58885eae47f4706d596b5aa2640b9a3f448c5b6e
VictorJoss <vramirezgarcia64@correo.unicordoba.edu.co>
2024-09-01 22:29:06 -0500

    initial commit

---
+++ src/main/java/com/victordev/emailreportgenerator/EmailReportGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.victordev.emailreportgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailReportGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailReportGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victordev/emailreportgenerator/EmailReportGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victordev.emailreportgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailReportGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
